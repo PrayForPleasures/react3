268f567275324cf48236367b158ade17ea0d416a
Dmitriy Yarchuck <dmitriyyarchuck@gmail.com>
2023-01-06 19:59:17 +0300

    react3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/header";
+import Sections from "./components/sections";
+import Container from "@material-ui/core/Container";
+import { makeStyles } from "@material-ui/core/styles";
+import backImage from "./images/backPhoto.jpg";
+
+const useStyles = makeStyles((theme) => ({}));
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ backgroundImage: `url(${backImage})` }}>
+      <Container>
+        <Header></Header>
+        <Sections></Sections>
+      </Container>
     </div>
   );
 }
