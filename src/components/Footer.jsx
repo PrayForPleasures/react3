10e20687c4b0e4323008d5ea7a12b257d83e6d4a
Dmitriy Yarchuck <dmitriyyarchuck@gmail.com>
2023-01-29 00:50:09 +0300

    r3

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import inst1 from "../images/inst1.png";
+import inst2 from "../images/inst2.png";
+import tt1 from "../images/tt1.png";
+import tt2 from "../images/tt2.png";
+import tel1 from "../images/tel1.png";
+import tel2 from "../images/tel2.png";
+import vk1 from "../images/vk1.png";
+import vk2 from "../images/vk2.png";
 
 const useStyles = makeStyles((theme) => ({
   flexFooter: {
@@ -9,6 +17,76 @@ const useStyles = makeStyles((theme) => ({
     color: "whitesmoke",
     fontSize: "12px",
   },
+  socialIcons: {
+    display: "flex",
+    justifyContent: "space-between",
+    listStyle: "none",
+  },
+  iconInst: {
+    display: "block",
+    marginLeft: "32px",
+    marginRight: "16px",
+    width: "20px",
+    height: "20px",
+    textDecoration: "none",
+    backgroundPosition: "center",
+    backgroundSize: "100%",
+    backgroundImage: `url(${inst1})`,
+    "&:hover": {
+      transition: "0.8s",
+      backgroundImage: `url(${inst2})`,
+    },
+  },
+  iconVK: {
+    display: "block",
+    marginRight: "16px",
+    width: "20px",
+    height: "20px",
+    textDecoration: "none",
+    backgroundPosition: "center",
+    backgroundSize: "100%",
+    backgroundImage: `url(${vk1})`,
+    "&:hover": {
+      transition: "0.8s",
+      backgroundImage: `url(${vk2})`,
+    },
+  },
+  iconTel: {
+    display: "block",
+    marginRight: "16px",
+    width: "20px",
+    height: "20px",
+    textDecoration: "none",
+    backgroundPosition: "center",
+    backgroundSize: "100%",
+    backgroundImage: `url(${tel1})`,
+    "&:hover": {
+      transition: "0.8s",
+      backgroundImage: `url(${tel2})`,
+    },
+  },
+  iconTT: {
+    display: "block",
+    marginRight: "32px",
+    width: "20px",
+    height: "20px",
+    textDecoration: "none",
+    backgroundPosition: "center",
+    backgroundSize: "100%",
+    backgroundImage: `url(${tt1})`,
+    "&:hover": {
+      transition: "0.8s",
+      backgroundImage: `url(${tt2})`,
+    },
+  },
+  flexRightsSS: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  backGradient: {
+    background: "linear-gradient(270deg, transparent, whitesmoke, transparent)",
+  },
 }));
 
 export default function Footer() {
@@ -17,8 +95,46 @@ export default function Footer() {
   return (
     <>
       <div className={classes.flexFooter}>
-        <div className={classes.mainTxt}>
-          &copy; All Rights Reserved, Dmitriy Yarchuck corp.
+        <div className={classes.flexRightsSS}>
+          <div className={classes.mainTxt}>
+            &copy; All Rights Reserved, Dmitriy Yarchuck corp.
+          </div>
+          <div className={classes.backGradient}>
+            <ul className={classes.socialIcons}>
+              <li className={classes.liStyle}>
+                <a
+                  className={classes.iconInst}
+                  href="#"
+                  title="..."
+                  target="_blank"
+                ></a>
+              </li>
+              <li className={classes.liStyle}>
+                <a
+                  className={classes.iconVK}
+                  href="#"
+                  title="..."
+                  target="_blank"
+                ></a>
+              </li>
+              <li className={classes.liStyle}>
+                <a
+                  className={classes.iconTel}
+                  href="#"
+                  title="..."
+                  target="_blank"
+                ></a>
+              </li>
+              <li className={classes.liStyle}>
+                <a
+                  className={classes.iconTT}
+                  href="#"
+                  title="..."
+                  target="_blank"
+                ></a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </>
